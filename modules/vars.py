a255d906282cf75f30ced44148ff3f89a02b95a3
Jaipajitendra123456 <167450527+Jaipajitendra123456@users.noreply.github.com>
2024-04-20 05:45:57 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "14453531"
-api_hash = "85965b03c611cd6e0625b9754437bd7c"
-bot_token = "7046220532:AAEj5SgKl2CzHwQDQ0liokQ2uf4Ebd6S9G4"
+api_id = "20860099"
+api_hash = "1b12641cd62b6d085da2f9e55d482ac6"
+bot_token = "6160808014:AAGY3N-dUtx2HjlOclxuwyxAqsjaN0OGjw4"
